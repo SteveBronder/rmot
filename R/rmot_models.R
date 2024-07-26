dd36bfc3e9b092809a8dcc129c0ff61cd4f93f80
Tess-LaCoil <tesslacoil@protonmail.com>
2024-07-26 20:16:37 +1000

    Removed linear regression material as it is not required.

--- R/rmot_models.R
+++ R/rmot_models.R
@@ -4,37 +4,23 @@
 #'
 #' @return named list that matches Stan model parameters
 #' @export
-#'
-#' @examples
-#' rmot_model("linear")
 
 rmot_model <- function(model=NULL){
 
-  #TODO: Need a mechanism to check model requested in one that is supported by rmot
+  #TODO: Need a mechanism to check model requested is one that is supported by rmot
 
   output <- switch(model,
-                   linear = rmot_lm(),
                    constant_single_ind = rmot_const_single_ind(),
                    constant_multi_ind = rmot_const_multi_ind(),
                    canham_single_ind = rmot_canham_single_ind(),
                    canham_multi_ind = rmot_canham_multi_ind(),
                    power_single_ind = rmot_power_single_ind(),
-                   power_multi_ind = rmot_power_multi_ind())
+                   power_multi_ind = rmot_power_multi_ind(),
+                   linear_single_ind = rmot_linear_single_ind())
 
   class(output) <- "rmot_object"
 
   return(output)
-}
-
-#' Data configuration template for linear model
-#' @keywords internal
-#' @noRd
-
-rmot_lm <- function(){
-  list(X = NULL,
-       Y = NULL,
-       N = NULL,
-       model = "linear")
 }
 
 #' Data configuration template for constant growth single individual model
@@ -126,3 +112,19 @@ rmot_power_multi_ind <- function(){
        y_0_obs = NULL,
        model = "power_multi_ind")
 }
+
+#' Data configuration template for linear growth single individual model
+#' @keywords internal
+#' @noRd
+#'
+rmot_linear_single_ind <- function(){
+  list(step_size = NULL,
+       n_obs = NULL,
+       y_obs = NULL,
+       obs_index = NULL,
+       time = NULL,
+       y_bar = NULL,
+       y_0_obs = NULL,
+       model = "linear_single_ind")
+}
+
